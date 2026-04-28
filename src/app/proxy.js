9205dc4f378a2a0b2f221a302d8d7b8fd0a294fa
salman <salmanibnriyaj@gmail.com>
2026-04-28 09:30:07 +0600

    all up to date

--- src/app/proxy.js
+++ src/app/proxy.js
@@ -1,19 +1,44 @@
+// import { auth } from "@/lib/auth";
+// import { headers } from "next/headers";
+// import { NextResponse } from "next/server";
+
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 
-export const proxy = async (request) => {
+// export const proxy = async (request) => {
+//   const session = await auth.api.getSession({
+//     headers: await headers(),
+//   });
+
+//   if (session) {
+//     return NextResponse.next();
+//   }
+//   return NextResponse.redirect(new URL("/login", request.url));
+// };
+
+// export const config = {
+//   matcher: ["/about"],
+// };
+
+
+
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
-  if (session) {
+  if(session){
     return NextResponse.next();
   }
   return NextResponse.redirect(new URL("/login", request.url));
-};
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request) { ... }
 
 export const config = {
-  matcher: "/about",
+  matcher: ['/about']
 };
